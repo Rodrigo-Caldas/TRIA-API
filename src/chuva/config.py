aeaf0f0fec05732a18eb75f08583469dda3b465c
Rodrigo-Caldas <rodrigocaldas@id.uff.br>
2025-05-21 03:21:43 -0300

    :wrench: Parent Path adicionado nos arquivos

--- src/chuva/config.py
+++ src/chuva/config.py
@@ -9,7 +9,7 @@ from pydantic_settings import BaseSettings
 class ConfiguracoesChuva(BaseSettings):
     """Configuracoes do back de chuva."""
 
-    caminho_csvs: Path = Path("arquivos")
+    caminho_csvs: Path = Path(__file__).parent / "arquivos"
 
     @property
     def obter_dataframe_chuva(self) -> pd.DataFrame:
@@ -21,12 +21,16 @@ class ConfiguracoesChuva(BaseSettings):
         pd.DataFrame
             Dataframe concatenados;
         """
-        arquivos_csv = list(self.caminho_csvs.glob("*.csv"))
-        df = pd.concat(
-            (pd.read_csv(arquivo) for arquivo in arquivos_csv), ignore_index=True
-        )
-        df = df.drop(columns="Unnamed: 0")
-        return df
+        try:
+            arquivos_csv = list(self.caminho_csvs.glob("*.csv"))
+            df = pd.concat(
+                (pd.read_csv(arquivo) for arquivo in arquivos_csv), ignore_index=True
+            )
+            df = df.drop(columns="Unnamed: 0")
+            return df
+        except Exception as e:
+            print("Algo errado aqui")
+            raise e
 
 
 config = ConfiguracoesChuva()
