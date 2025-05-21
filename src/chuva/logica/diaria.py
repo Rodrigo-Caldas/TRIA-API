539b03f535c84db74cc5f83f572101ca82e1fe37
Rodrigo-Caldas <rodrigocaldas@id.uff.br>
2025-05-21 03:21:13 -0300

    :hammer: Refatoração de lógica

--- src/chuva/logica/diaria.py
+++ src/chuva/logica/diaria.py
@@ -3,13 +3,13 @@
 from typing import Literal
 
 import pandas as pd
-from modelos import DatasDisponiveis
 
-from config import config
+from src.chuva.config import config
+from src.chuva.modelos import DatasDisponiveis
 
 
 def buscar_datas_disponiveis(
-    codigo: Literal[1547079, 2244107, 3051043, 9999999],
+    codigo: Literal["1547079", "2244107", "3051043", "9999999"],
 ) -> DatasDisponiveis:
     """
     Busca as datas disponíveis a partir de um código de estação.
@@ -24,13 +24,14 @@ def buscar_datas_disponiveis(
     DatasDisponiveis
         Datas disponíveis para o código escolhido.
     """
+    codigo_int = int(codigo)
     df = config.obter_dataframe_chuva
-    df["datas"] = pd.to_datetime(df["data"])
 
-    df_filtrado = df.loc[df["codigo"] == codigo]
+    df_filtrado = df.loc[df["codigo"] == codigo_int].copy()
+    df_filtrado["datas"] = pd.to_datetime(df_filtrado["data"])
     df_filtrado = df_filtrado.drop(columns=["chuva", "codigo", "data"])
 
-    datas = {"datas": list(df["datas"])}
+    datas = {"datas": list(df_filtrado["datas"])}
 
     objeto = DatasDisponiveis(**datas)
 
